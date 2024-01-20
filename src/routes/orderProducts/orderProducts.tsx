7b7e16574297b5c4ed18936f207f7cacdc8991da
Emerson <emersonhzulian@gmail.com>
2024-01-20 13:55:21 -0300

    V0

--- src/routes/orderProducts/orderProducts.tsx
+++ src/routes/orderProducts/orderProducts.tsx
@@ -1,18 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import { Api } from "../../apiClient/Api";
-import { OrderDTO, ProductDTO } from "../../apiClient/data-contracts";
+import {
+  OrderDTO,
+  OrderProductDTO,
+  ProductDTO,
+} from "../../apiClient/data-contracts";
 import { OrderComponent } from "../../components/order/orderComponent";
 
 export async function loader({ request, params }): Promise<{
   order: OrderDTO;
-  products: ProductDTO[];
+  orderProducts: OrderProductDTO[];
 }> {
   const orderId = params.comandaId;
   const api = Api.Instance;
   const order = (await api.ordersDetail(orderId)).data;
-  const products = (await api.productsList()).data;
+  const orderProducts = (await api.orderProductsList()).data;
 
-  return { order, products };
+  return { order, orderProducts };
 }
 
 export async function action({ request, params }) {}
